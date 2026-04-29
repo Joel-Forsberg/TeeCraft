585d2d4b0f495d71404c18173bbcd2067a2f99b1
Joel-Forsberg <joeleliasforsberg2@gmail.com>
2026-04-29 11:54:49 +0200

    Added cart and cart item models

---
+++ TeeCraft.API/Models/Cart.cs
@@ -0,0 +1,14 @@
+﻿namespace TeeCraft.API.Models;
+
+public class Cart
+{
+    public int CartId { get; set; }
+
+    public int CustomerId { get; set; }
+
+    public Customer Customer { get; set; } = null!;
+
+    public DateTime CreatedAt { get; set; } = DateTime.UtcNow;
+
+    public ICollection<CartItem> CartItems { get; set; } = new List<CartItem>();
+}
---
+++ TeeCraft.API/Models/CartIteam.cs
@@ -0,0 +1,16 @@
+﻿namespace TeeCraft.API.Models;
+
+public class CartItem
+{
+    public int CartItemId { get; set; }
+
+    public int CartId { get; set; }
+
+    public Cart Cart { get; set; } = null!;
+
+    public int ProductVariantId { get; set; }
+
+    public ProductVariant ProductVariant { get; set; } = null!;
+
+    public int Quantity { get; set; }
+}
