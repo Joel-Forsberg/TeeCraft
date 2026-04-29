52d17422f68db622b54ca2f0ff7a458505d84e4b
Joel-Forsberg <joeleliasforsberg2@gmail.com>
2026-04-29 11:53:02 +0200

    Added product category and variant models

---
+++ TeeCraft.API/Models/Product.cs
@@ -0,0 +1,18 @@
+﻿namespace TeeCraft.API.Models;
+
+public class Product
+{
+    public int ProductId { get; set; }
+
+    public string Name { get; set; } = string.Empty;
+
+    public string Description { get; set; } = string.Empty;
+
+    public decimal BasePrice { get; set; }
+
+    public int CategoryId { get; set; }
+
+    public Category Category { get; set; } = null!;
+
+    public ICollection<ProductVariant> ProductVariants { get; set; } = new List<ProductVariant>();
+}
---
+++ TeeCraft.API/Models/ProductVariant.cs
@@ -0,0 +1,24 @@
+﻿namespace TeeCraft.API.Models;
+
+public class ProductVariant
+{
+    public int ProductVariantId { get; set; }
+
+    public int ProductId { get; set; }
+
+    public Product Product { get; set; } = null!;
+
+    public string Color { get; set; } = string.Empty;
+
+    public string Size { get; set; } = string.Empty;
+
+    public string Fit { get; set; } = string.Empty;
+
+    public int StockQuantity { get; set; }
+
+    public decimal Price { get; set; }
+
+    public ICollection<CartItem> CartItems { get; set; } = new List<CartItem>();
+
+    public ICollection<OrderItem> OrderItems { get; set; } = new List<OrderItem>();
+}
