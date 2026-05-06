9997ce8b91dc6a736ed58657c0360351a0d8ec23
Joel-Forsberg <joeleliasforsberg2@gmail.com>
2026-05-06 10:45:43 +0200

    Added Products API with GET endpoints

---
+++ TeeCraft.API/Controllers/ProductsController.cs
@@ -0,0 +1,43 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using TeeCraft.API.Data;
+using TeeCraft.API.Models;
+
+namespace TeeCraft.API.Controllers;
+
+[Route("api/[controller]")]
+[ApiController]
+public class ProductsController : ControllerBase
+{
+    private readonly AppDbContext _context;
+
+    public ProductsController(AppDbContext context)
+    {
+        _context = context;
+    }
+
+    // GET: api/products
+    [HttpGet]
+    public async Task<ActionResult<IEnumerable<Product>>> GetProducts()
+    {
+        return await _context.Products
+            .Include(p => p.ProductVariants)
+            .ToListAsync();
+    }
+
+    // GET: api/products/1
+    [HttpGet("{id}")]
+    public async Task<ActionResult<Product>> GetProduct(int id)
+    {
+        var product = await _context.Products
+            .Include(p => p.ProductVariants)
+            .FirstOrDefaultAsync(p => p.ProductId == id);
+
+        if (product == null)
+        {
+            return NotFound();
+        }
+
+        return product;
+    }
+}
